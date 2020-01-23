8a05769a83a2670ea05ea105d039e38a7ec0baa1
ssuchir <ssuchir@gmail.com>
2020-01-23 00:49:35 -0500

    Added Search form with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (<div className="ui container" style={{ marginTop: '10px'}}><SearchBar /></div>);
-};
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 5fd3bc0b804427caaac4b69f200c8df907453877a82180abb9775249dc4ed1fc'
+      }
+    });
+  }
+
+  render () {
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+      <SearchBar onSubmit={this.onSearchSubmit} /></div>);
+  }
+}
 
 export default App;
