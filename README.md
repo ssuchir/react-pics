c77e27a50065ea2da628b93343419a758d98bbb1
ssuchir <ssuchir@gmail.com>
2020-02-08 19:15:06 -0500

    Changes publish settings

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Image Search App
+
+Built with Unspash API
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/components/HeaderDiv.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+const HeaderDiv = () => (
+  <Header as='h3' dividing>
+    Dividing Header
+  </Header>
+)
+
+export default HeaderDiv
